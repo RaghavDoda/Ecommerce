13cb2903c706e787e094cf4551f35511417f7cf1
Pratham goyal <95137153+Pratham1504@users.noreply.github.com>
2023-03-10 01:30:36 +0530

    added footer and login page

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -2,19 +2,19 @@ const Profile = () =>{
     return (
         <div className="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
             <div className="offcanvas-header">
-                <h4 style={{textShadow:"none"}} >Profile</h4>
+                <h4 style={{textShadow:"none",fontFamily:"sans-serif"}} >Profile</h4>
                 <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" style={{fontSize:"60%"}}></button>
             </div>
             <div className="offcanvas-body" >
-                <span style={{textShadow:"none" ,fontSize:"70%", justifyContent:"center"}}><strong>Name:</strong>Raghav doda</span>
-                <br />
-                <span style={{textShadow:"none",fontSize:"70%",justifyContent:"center"}}><strong>Phone Number:</strong>+91-7527902363</span>
-                <br />
-                <span style={{textShadow:"none",fontSize:"70%",justifyContent:"center"}}><strong>email:</strong>raghav@gmail.com</span>
-                <br />
-                <a href="/" style={{textShadow:"none",fontSize:"70%",justifyContent:"center"}}><strong>Login</strong></a>
-                <br />
-                <a href="/" style={{textShadow:"none",fontSize:"70%",justifyContent:"center"}}><strong>Logout</strong></a>
+                <span style={{textShadow:"none" ,fontSize:"70%", justifyContent:"center",fontFamily:"monospace"}}><strong>Name:</strong>Raghav doda</span>
+                <hr />
+                <span style={{textShadow:"none",fontSize:"70%",justifyContent:"center",fontFamily:"monospace"}}><strong>Phone Number:</strong>+91-7527902363</span>
+                <hr />
+                <span style={{textShadow:"none",fontSize:"70%",justifyContent:"center",fontFamily:"monospace"}}><strong>email:</strong>raghav@gmail.com</span>
+                <hr />
+                <a class="btn btn-dark" href="/" role="button">Login</a>
+                <hr />
+                <a class="btn btn-dark" href="/" role="button">Logout</a>
             </div>
         </div>
     )
