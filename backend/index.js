9d862fd9db2a9905995bea82235b6a955db3f78d
Raghav <raghav03.doda@gmail.com>
2023-03-10 11:54:37 +0530

    Products model and routes added

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+const express = require('express');
+const productRouts = require('./routes/products')
+// const userRouts = require('./Routes/user')
+
+//express app
+const app = express();
+
+//middleware
+app.use(express.json())
+app.use((req,res,next)=>{
+        console.log(req.path,req.method)
+        next()
+})
+
+//routes
+app.use('/products',productRouts)
+// app.use('/api/user',userRouts) 
+
+
+//connect to db
+// mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+        
+    //listen for requests
+    app.listen(process.env.PORT,()=>{
+        console.log(`connected to db & listening to the port ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log(err);
+})
---
+++ backend/model/Product.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const productSchema = new Schema({
+    title : {
+        type : String,
+        required : true
+    },
+    price : {
+        type : Number ,
+        required : true
+    },
+    rating : {
+        type : Number,
+        default:0
+    },
+    details: {
+        type : String,
+        required:true
+    },
+    company_id:{
+        type:String
+    }
+})
+
+module.exports = mongoose.model('products',productSchema)
